e818e0b312b0489f66052dda6b29d027234a5915
Adrian Nowakowski <anowakowski@mailc.net>
2020-05-11 13:44:57 +0200

    first part of catalog infrastrucutre

--- Fastretro.API/Controllers/WeatherForecastController.cs
+++ Fastretro.API/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace fastretro_api.Controllers
+namespace Fastretro.API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- Fastretro.API/Startup.cs
+++ Fastretro.API/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace fastretro_api
+namespace Fastretro.API
 {
     public class Startup
     {
