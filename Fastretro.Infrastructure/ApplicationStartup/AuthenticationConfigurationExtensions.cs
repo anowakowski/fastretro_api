1a866e74078389b0f1b52b2643290168b63d2979
Adrian Nowakowski <anowakowski@mailc.net>
2020-05-14 21:50:15 +0200

    added auth config extension

---
+++ Fastretro.Infrastructure/ApplicationStartup/AuthenticationConfigurationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Fastretro.Infrastructure.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fastretro.Infrastructure.ApplicationStartup
+{
+    public static class AuthenticationConfigurationExtensions
+    {
+        public static void AddAndConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+
+                var issuerKeyProvider = new FirebaseIssuerKeyProvider(configuration.GetSection("Authentication:PublicKeysRequestUri").Value);
+                var signingKeys = issuerKeyProvider.GetSigningKeys().Result;
+
+                options.Authority = configuration.GetSection("Authentication:Authority").Value;
+                options.TokenValidationParameters =
+                    new FirebaseTokenValidationParameters(
+                        configuration.GetSection("Authentication:FirebaseProjectId").Value,
+                        signingKeys,
+                        configuration.GetSection("Authentication:ValidIssuer").Value);
+            });
+        }
+    }
+}
--- Fastretro.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Fastretro.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
