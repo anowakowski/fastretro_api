756e1df1c613131e379547aa21d40ed360f7805a
Adrian Nowakowski <anowakowski@mailc.net>
2020-05-20 21:52:04 +0200

    prepare db sets

--- Fastretro.API/Data/DataContext.cs
+++ Fastretro.API/Data/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Fastretro.API.Data.Domain;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,9 @@ namespace Fastretro.API.Data
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<CurrentUserInRetroBoard> CurrentUserInRetroBoards { get; set; }
+
+        public DbSet<FirebaseUser> FirebaseUsers { get; set; }
     }
 }
