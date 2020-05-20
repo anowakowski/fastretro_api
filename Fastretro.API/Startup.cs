48b9ebba32e61cf3a3458fe5d04cb27e0f8b9a64
Adrian Nowakowski <anowakowski@mailc.net>
2020-05-20 22:09:08 +0200

    added connection set

--- Fastretro.API/Startup.cs
+++ Fastretro.API/Startup.cs
@@ -1,6 +1,8 @@
+using Fastretro.API.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,7 @@ namespace Fastretro.API
         {
             services.AddControllers();
             services.AddCors();
+            services.AddDbContext<DataContext>(db => db.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly(typeof(Startup).Namespace)));
             services
                 .AddAuthentication(x =>
                 {
