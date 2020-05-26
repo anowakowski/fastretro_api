0b443e977153491890423ed131adf793d5679e2e
Adrian Nowakowski <anowakowski@mailc.net>
2020-05-26 10:35:03 +0200

    next part of migration

---
+++ Fastretro.API/Migrations/20200526083419_ChosenUrlForUsr.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fastretro.API.Migrations
+{
+    public partial class ChosenUrlForUsr : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ChosenAvatarUrl",
+                table: "FirebaseUsersData",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChosenAvatarUrl",
+                table: "FirebaseUsersData");
+        }
+    }
+}
