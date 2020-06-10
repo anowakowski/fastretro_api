070a7b93e8de0703c63fa96c8039dc5aa82a4d15
Adrian Nowakowski <anowakowski@mailc.net>
2020-06-10 13:30:25 +0200

    added migrations

---
+++ Fastretro.API/Migrations/20200610112902_userInTeamsEntity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fastretro.API.Migrations
+{
+    public partial class userInTeamsEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsersInTeams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserFirebaseDocId = table.Column<string>(nullable: true),
+                    TeamFirebaseDocId = table.Column<string>(nullable: true),
+                    WorkspaceFirebaseDocId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsersInTeams", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersInTeams");
+        }
+    }
+}
