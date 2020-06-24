d37c7f79719b91f1e96102449abd30f01e853310
Adrian Nowakowski <anowakowski@mailc.net>
2020-06-24 23:49:29 +0200

    add migration

---
+++ Fastretro.API/Migrations/20200624214903_RetroBoardStatusEntity.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fastretro.API.Migrations
+{
+    public partial class RetroBoardStatusEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
