8ab25c1aec1e967b952855cee8af036343a7bcb2
Adrian Nowakowski <anowakowski@mailc.net>
2020-06-30 17:25:56 +0200

    prepare first part to add userNotification

--- Fastretro.API/Services/IUserNotificationServices.cs
+++ Fastretro.API/Services/IUserNotificationServices.cs
@@ -1,6 +1,10 @@
+using System.Threading.Tasks;
+using Fastretro.API.Models;
+
 namespace Fastretro.API.Services
 {
     public interface IUserNotificationServices
     {
+        Task SetUserNotification(UserNotificationModel model);
     }
 }
--- Fastretro.API/Services/UserNotificationServices.cs
+++ Fastretro.API/Services/UserNotificationServices.cs
@@ -1,7 +1,33 @@
+using System.Threading.Tasks;
+using Fastretro.API.Data;
+using Fastretro.API.Data.Domain;
+using Fastretro.API.Data.Repositories;
+using Fastretro.API.Models;
+
 namespace Fastretro.API.Services
 {
     public class UserNotificationServices : IUserNotificationServices
     {
-        
+        private readonly IRepository<UserNotification> repository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public UserNotificationServices(IRepository<UserNotification> repository, IUnitOfWork unitOfWork)
+        {
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+        public async Task SetUserNotification(UserNotificationModel model)
+        {
+            var userNotification = new UserNotification 
+            {
+                UserWantToJoinFirebaseId = model.UserWantToJoinFirebaseId,
+                CreatorUserFirebaseId = model.CreatorUserFirebaseId,
+                WorkspceWithRequiredAccessFirebaseId = model.WorkspceWithRequiredAccessFirebaseId,
+                NotyficationType = "WorkspaceWithRequiredAccess"
+            };
+
+            await this.repository.AddAsync(userNotification);
+            await this.unitOfWork.CompleteAsync();
+        }
     }
 }
