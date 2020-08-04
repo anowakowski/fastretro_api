23ddf062daaac55bd2489b7091e5d79a58157e05
Adrian Nowakowski <anowakowski@mailc.net>
2020-08-04 11:34:28 +0200

    fixed migrations and db context

---
+++ Fastretro.API/Migrations/20200804092352_RetroBoardEntityToUpper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fastretro.API.Migrations
+{
+    public partial class RetroBoardEntityToUpper : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_retroBoards",
+                table: "retroBoards");
+
+            migrationBuilder.RenameTable(
+                name: "retroBoards",
+                newName: "RetroBoards");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_RetroBoards",
+                table: "RetroBoards",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_RetroBoards",
+                table: "RetroBoards");
+
+            migrationBuilder.RenameTable(
+                name: "RetroBoards",
+                newName: "retroBoards");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_retroBoards",
+                table: "retroBoards",
+                column: "Id");
+        }
+    }
+}
