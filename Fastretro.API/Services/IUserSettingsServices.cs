6b4a4f482d61f55bec0da9b2e1d618fa9f1b53f1
Adrian Nowakowski <anowakowski@mailc.net>
2020-09-30 15:19:44 +0200

    prepare add new user settings

--- Fastretro.API/Services/IUserSettingsServices.cs
+++ Fastretro.API/Services/IUserSettingsServices.cs
@@ -1,6 +1,13 @@
-﻿namespace Fastretro.API.Services
+﻿using Fastretro.API.Data.Domain;
+using Fastretro.API.Models;
+using System.Threading.Tasks;
+
+namespace Fastretro.API.Services
 {
     public interface IUserSettingsServices
     {
+        Task AddNewUserSettings(UserSettingsModel model);
+        Task UpdateUserSettings(UserSettingsModel model);
+        Task<UserSettings> GetUserSettings(UserSettingsModel model);
     }
 }
--- Fastretro.API/Services/UserSettingsServices.cs
+++ Fastretro.API/Services/UserSettingsServices.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Fastretro.API.Data;
+using Fastretro.API.Data.Domain;
+using Fastretro.API.Data.Repositories;
+using Fastretro.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +11,35 @@ namespace Fastretro.API.Services
 {
     public class UserSettingsServices : IUserSettingsServices
     {
+        private readonly IRepository<UserSettings> repository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public UserSettingsServices(IRepository<UserSettings> repository, IUnitOfWork unitOfWork)
+        {
+            this.repository = repository;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task AddNewUserSettings(UserSettingsModel model)
+        {
+            var userSettings = new UserSettings
+            {
+                UserFirebaseDocId = model.UserFirebaseDocId,
+                ChosenImageBackgroundName = model.ChosenImageBackgroundName
+            };
+
+            await this.repository.AddAsync(userSettings);
+            await this.unitOfWork.CompleteAsync();
+        }
+
+        public Task<UserSettings> GetUserSettings(UserSettingsModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateUserSettings(UserSettingsModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
