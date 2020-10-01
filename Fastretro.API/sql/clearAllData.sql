024ee90cc22fa05ac7444a6e50eb94b640f868d3
Adrian Nowakowski <anowakowski@mailc.net>
2020-10-01 16:22:27 +0200

    added clear all data script

---
+++ Fastretro.API/sql/clearAllData.sql
@@ -0,0 +1,29 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [Id]
+      ,[RetroBoardId]
+  FROM [anowakowski_retrodb].[anowakowski_retrousr].[CurrentUserInRetroBoards]
+
+  
+
+
+  select * from FirebaseUsersData
+
+  truncate table FirebaseUsersData
+  delete from [anowakowski_retrodb].[anowakowski_retrousr].[CurrentUserInRetroBoards]
+  truncate table CurrentUserVotes
+  truncate table MergedRetroBoardCards
+  truncate table RetroBoardActionCards
+  truncate table RetroBoardAdditionalInfos
+  delete from RetroBoardCards
+  delete from RetroBoardCardMergedGroups
+  truncate table RetroBoards
+  truncate table RetroBoardOptions
+  truncate table RetroBoardStatuses
+
+  truncate table RetroBoardOptions
+  truncate table RetroBoardOptions
+  truncate table RetroBoardOptions
+
+  RetroBoardActionCards
+
+
